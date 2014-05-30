87052d557860d9c93927ad5bb077b82771de7cc3
Brent Shepherd <thenbrent@gmail.com>
2014-05-30 11:32:52 -0700

    Add /forums/<id>/topics route

--- lib/class-bbp-json-forums.php
+++ lib/class-bbp-json-forums.php
@@ -35,6 +35,24 @@ class BBP_JSON_Forums extends WP_JSON_CustomPostType {
 	protected $type = 'forum';
 
 	/**
+	 * Register the page-related routes
+	 *
+	 * @param array $routes Existing routes
+	 * @return array Modified routes
+	 */
+	public function register_routes( $routes ) {
+
+		$routes = parent::register_routes( $routes );
+
+		// Add topic's replies route
+		$routes[ $this->base . '/(?P<id>\d+)/topics'] = array(
+			array( array( $this, 'get_posts_by_parent' ), WP_JSON_Server::READABLE ),
+		);
+
+		return $routes;
+	}
+
+	/**
 	 * Prepare post data
 	 *
 	 * @param array $post The unprepared post data
@@ -51,5 +69,18 @@ class BBP_JSON_Forums extends WP_JSON_CustomPostType {
 			$_post['meta']['links']['up'] = json_url( $this->base . '/' . get_page_uri( (int) $post['post_parent'] ) );
 
 		return apply_filters( 'json_prepare_page', $_post, $post, $context );
+	}
+
+	/**
+	 * Get the topics that a user created
+	 *
+	 * Kind of like bbp_get_user_topics_started()
+	 *
+	 * @see WP_JSON_Posts::get_posts()
+	 */
+	public function get_posts_by_parent( $id, $context = 'view', $page = 1 ) {
+		global $bbp_json_topics;
+
+		return $bbp_json_topics->get_posts( array( 'post_parent' => $id ), $context );
 	}
 }
